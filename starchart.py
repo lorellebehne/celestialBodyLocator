a593227b93d1fa1ebf91eaac5f99494bf9e45202
lorellebehne <behnlo01@luther.edu>
2018-04-23 17:52:31 -0500

    Add files via upload
    Final

--- starchart.py
+++ starchart.py
@@ -1,29 +1,61 @@
-from flask import Flask, request
-
-import requests as requests #may need to pip install requests
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import requests
 
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 key = "H6948P-WPKW3GLAKW" 
-
-@app.route("/search/<string:body>/<string:day>/<string:month>/<string:year>", methods=["GET"])
-def findBody(body,day, month,year):
+long = 0.0
+@app.route("/search", methods=["GET"])
+def findBody():
 	print("got date")
+	year = request.args["year"]
+	month = request.args["month"]
+	day = request.args["day"]
+	body = request.args["body"]
 	date="{}-{}-{}".format(year, month, day)
-	st = format(body,date)
-	try:
-		res = request.get("http://api.wolframalpha.com/v2/query?input={}+position+{}&appid={}&format=plaintext&podtitle=Result&formattype=plaintext&assumption=DateOrder_**Year.Month.Day--".format(body,date,key))
-		loc = res.split("\n")[4]
-		print(loc)
-		return jsonify(loc)
-	except:
-		console.log("it is not a da working")
-		
+	res = requests.get("http://api.wolframalpha.com/v2/query?input={}+position+{}&appid={}&format=plaintext&podtitle=Result&formattype=plaintext&assumption=DateOrder_**Year.Month.Day--".format(body,date,key)).text.split("\n")
+	#loc = res.split("\n")[4]
+	latHMS = []
+	change = False
+	str = ""
+	print(res[24])
+	for i in res[24][32:]:
+		if i in "-.1234567890":
+			str+=i
+			change = False
+		else:
+			if not change:
+				latHMS.append(str)
+			str = ''
+			change = True
 	
-
- 
+	#outs : <plaintext>right ascension | 23^h 14^m 53.2^s
+	longHMS = []
+	change = False
+	str = ""
+	for i in res[25][14:]:
+		if i in "-.1234567890":
+			str+=i
+			change = False
+		else:
+			if not change:
+				longHMS.append(str)
+			str = ''
+			change = True
+			
+	lat = float(latHMS[0])*15+float(latHMS[1])/60+float(latHMS[2])/60/60
+	if lat > 180:
+		lat = lat -360
+	long = float(longHMS[0])+float(longHMS[1])/60+float(longHMS[2])/60/60
+	print(lat, long)
+	#outs : declination | -3DEGREE SYMBOL 27&apos; 20&quot;</plaintext>
+	#return(res[25])
+	
+	return jsonify(latHMS,longHMS)
 
 if __name__ == '__main__':
 
